74e79a14ea2d7d898b0c2cbcfe98a5ca10cafcbc
Leonardo Felipe Takao Hirata <leo.fthirata@gmail.com>
2025-04-09 00:09:20 -0300

    add task timer to restart timer

--- learning/dkm/wd-timer/src/dkm.c
+++ learning/dkm/wd-timer/src/dkm.c
@@ -13,11 +13,9 @@ uint32_t timeout = 1000;
 bool control = false;
 WDOG_ID timer;
 
-void timer_handler(int param)
+void timer_handler()
 {
-	// printf("Timer triggered: param %d\n", param);
 	control = true;
-	wdStart(timer, DELAY_MS(1000), (FUNCPTR)timer_handler, param);
 }
 
 int timer_config()
@@ -28,25 +26,38 @@ int timer_config()
 		return -1;
 	}
 
-	wdStart(timer, DELAY_MS(1000), (FUNCPTR)timer_handler, 0x100);
+	wdStart(timer, DELAY_MS(1000), (FUNCPTR)timer_handler, 0);
 
 	return 0;
+}
+
+void timer_task()
+{
+	printf("Timer task started!\n");
+
+	timer_config();
+
+	int count = 0;
+	while (count < 20) {
+		if (control) {
+			count++;
+			printf("Timer triggered\n");
+			control = false;
+			wdStart(timer, DELAY_MS(1000), (FUNCPTR)timer_handler, 0);
+		}
+		taskDelay(DELAY_MS(1));
+	}
+
+	taskDelete(NULL);
 }
 
 int main()
 {
 	printf("DKM WD Timer\n");
 
-	timer_config();
+	TASK_ID timer_task_id = taskSpawn("timer_task", 10, 0, 200, (FUNCPTR)timer_task, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
-	int count = 0;
-	while (count < 20) {
-		count++;
-		printf("main while...\n");
-		if (control) {
-			printf("Timer triggered\n");
-			control = false;
-		}
+	while (taskIdVerify(timer_task_id) == OK) {
 		taskDelay(DELAY_MS(500));
 	}
 
